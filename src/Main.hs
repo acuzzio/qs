20a998657f74ddd66251f50f549463566fc45def
acuzzio <acuzzio@gmail.com>
2014-07-20 14:51:18 +0200

    rs

--- src/Main.hs
+++ src/Main.hs
@@ -8,12 +8,12 @@ import System.IO
 import System.Process
 import System.ShQQ
 
-qstatOutput = "output.qstat"
+--qstatOutput = "output.qstat"
 
 main = do
   args <- getArgs
---  a <- readShell "qstat -f"
-  a <- readFile qstatOutput 
+  a <- readShell "qstat -f"
+--  a <- readFile qstatOutput 
   Just win <- hSize stdout
   if a == "" 
      then putStrLn "\nNo Jobs on Queue !!\n"
